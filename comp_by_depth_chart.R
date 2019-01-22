0955d458d0a312d93a9d282217905fe12fc88e45
Josh Hermsmeyer <joshua.hermsmeyer@fivethirtyeight.com>
2019-01-22 08:22:26 -0800

    Update league average line to include 2017 and 2018.

--- comp_by_depth_chart.R
+++ comp_by_depth_chart.R
@@ -7,12 +7,6 @@ library(tidyverse)
 
 pass_plays <- read_csv("data/__pass_plays.csv")
 
-### Define a function that plots a curve approximating the
-### league average completion percentage at each depth from 2009-2016
-fun1 <- function(p) {
-   0.00028 * p ^ 2 - 0.0239 * p + 0.787
-}
-
 ### Elias charts dumps throw aways into the 0 air yards bucket.
 ### This biases the data when binning, so we remove it.
 pass_plays_no_spikes_qb_hit <- pass_plays %>%
@@ -20,15 +14,29 @@ pass_plays_no_spikes_qb_hit <- pass_plays %>%
    filter(spike == 0,
           qb_hits >= 1)
 
+pass_plays_no_spikes_no_hits <- pass_plays %>%
+   mutate(spike = ifelse(str_detect(description, "spiked"), 1, 0)) %>%
+   filter(spike == 0,
+          qb_hits == 0)
+
 ggplot(pass_plays_no_spikes_qb_hit, aes(x = air_yards, y = passing_cmp)) +
    theme_minimal() +
    theme(plot.title=element_text(size=16, hjust=0, vjust=-1),
          plot.subtitle=element_text(size=12, hjust=0)) +
-   stat_function(fun = fun1, color = '#FFB704') +
    geom_smooth(
       fill = '#CCCCCC',
       alpha = .2,
       color = '#018417',
+      size = .6,
+      span = .5,
+      se = FALSE
+   ) +
+   geom_smooth(
+      data = pass_plays_no_spikes_no_hits,
+      aes(y = passing_cmp, x = air_yards),
+      fill = '#CCCCCC',
+      alpha = .2,
+      color = '#FFB704',
       size = .6,
       span = .5,
       se = FALSE
